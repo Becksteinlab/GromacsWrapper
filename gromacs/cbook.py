883f6d707aa9a238143b30337b56de05667e89df
Oliver Beckstein <orbeckst@gmail.com>
2009-06-15 22:40:47 +0000

    edit_mdp() function added
    git-svn-id: svn+ssh://gonzo.med.jhmi.edu/scratch/svn/woolf_repository/users/oliver/Library/GromacsWrapper@3571 df5ba8eb-4b0b-0410-8c14-c10f23b0129c

--- gromacs/cbook.py
+++ gromacs/cbook.py
@@ -26,6 +26,8 @@ also serve as canonical examples for how to do simple things.
 # default) ... or I can write some meta(??) class to do this nicely
 
 import re
+import tempfile
+import shutil
 
 import gromacs
 from gromacs import GromacsError
@@ -55,3 +57,67 @@ def grompp_qtot(*args, **kwargs):
             break
     return qtot
 
+
+def edit_mdp(mdp, new_mdp=None, **substitutions):
+    """Change values in a Gromacs mdp file.
+
+    edit_mdp('md.mdp', new_mdp='long_md.mdp', nsteps=100000, nstxtcout=1000, lincs_iter=2)
+
+    Parameters and values are supplied as substitutions, eg nsteps=1000. Note
+    that dashes have to be replaced by an underscore.
+    
+    By default the template mdp file is **overwritten in place**.
+
+    :Arguments:
+    mdp             filename of input (and output filename of new_mdp=None)
+    new_mdp         filename of output mdp file [None]
+    substitutions   parameter=value pairs, where parameter is defined by Gromacs
+
+    :Returns:
+    List of parameters that have NOT been substituted.
+
+    :Bugs: 
+    * Parameters *aa_bb* and *aa-bb* are considered the same (but should not be a problem).
+    * This code is more compact in ``Perl`` as one can use ``s///`` operators:
+          s/^(\s*$key\s*=\s*).*/$1${val}/
+    """
+    if new_mdp is None:
+        new_mdp = mdp
+    params = substitutions.keys()[:]   # list will be reduced for each match
+
+    def demangled(p):
+        return p.replace('_', '[-_]')  # must catch either - or _
+
+    patterns = dict([(parameter,
+                      re.compile("""\
+                       (?P<assignment>\s*%s\s*=\s*)  # everything before the value
+                       (?P<value>[^;]*)              # value (stop before comment=;)
+                       (?P<comment>\s*;.*)?          # optional comment           
+                       """ % demangled(parameter), re.VERBOSE))
+                     for parameter in substitutions])
+
+    target = tempfile.TemporaryFile()
+    with open(mdp) as src:
+        for line in src:
+            new_line = line.strip()
+            for p in params[:]:
+                m = patterns[p].match(line)
+                if m:
+                    # I am too stupid to replace a specific region in the string so I rebuild it
+                    # (matching a line and then replacing value requires TWO re calls)
+                    if m.group('comment') is None:
+                        comment = ''
+                    else:
+                        comment = " "+m.group('comment')
+                    new_line = "".join([m.group('assignment'), 
+                                        str(substitutions[p]), 
+                                        comment])
+                    params.remove(p)
+                    break
+            target.write(new_line+'\n')
+    target.seek(0)
+    # XXX: Is there a danger of corrupting the original mdp if something went wrong?
+    with open(new_mdp, 'w') as final:
+        shutil.copyfileobj(target, final)
+    target.close()
+    return params
