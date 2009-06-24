3fa413a6a483adcc37ff8d7c949c063a2f9c70dd
Oliver Beckstein <orbeckst@gmail.com>
2009-06-24 02:06:21 +0000

    combined distribution script; added licenses
    git-svn-id: svn+ssh://gonzo.med.jhmi.edu/scratch/svn/woolf_repository/users/oliver/Library/GromacsWrapper@3640 df5ba8eb-4b0b-0410-8c14-c10f23b0129c

--- scripts/distribute.sh
+++ scripts/distribute.sh
@@ -1,4 +1,35 @@
-#!/bin/sh
-python setup.py sdist
-python setup.py bdist_egg
-rsync -v dist/* /sansom/public_html/sbcb/oliver/download/Python/
+#!/bin/bash
+
+SERVERDIR=/sansom/public_html/sbcb/oliver
+PACKAGES=$SERVERDIR/download/Python
+DOCS=$SERVERDIR/software/GromacsWrapper
+
+distribution () {
+  python setup.py sdist
+  python setup.py bdist_egg
+  rsync -v dist/* $PACKAGES
+}
+
+make_epydocs() {
+  epydoc -v -o doc/epydoc --html --name=GromacsWrapper \
+         --url=http://sbcb.bioch.ox.ac.uk/oliver/software/GromacsWrapper/ \
+         gromacs gromacs/analysis/plugins/ vmd/
+}
+
+make_sphinx () {
+  (cd doc/sphinx && make html)
+}
+
+docs () {
+  make_epydocs 
+  make_sphinx
+  rsync -vrP doc/epydoc $DOCS
+  rsync -vrP doc/sphinx/build/html $DOCS
+}
+
+
+
+distribution
+docs
+
+  
--- scripts/make_epydoc.sh
+++
@@ -1,1 +0,0 @@
-epydoc -v -o doc/epydoc --html --name=GromacsWrapper --url=http://sbcb.bioch.ox.ac.uk/oliver/software/GromacsWrapper/  gromacs gromacs/analysis/plugins/ vmd/
