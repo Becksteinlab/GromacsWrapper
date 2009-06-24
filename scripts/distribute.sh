8ea3b358ca621913d16e5fdaa6323e14f0feead2
Oliver Beckstein <orbeckst@gmail.com>
2009-06-24 17:19:29 +0000

    neater dist script
    git-svn-id: svn+ssh://gonzo.med.jhmi.edu/scratch/svn/woolf_repository/users/oliver/Library/GromacsWrapper@3649 df5ba8eb-4b0b-0410-8c14-c10f23b0129c

--- scripts/distribute.sh
+++ scripts/distribute.sh
@@ -4,32 +4,60 @@ SERVERDIR=/sansom/public_html/sbcb/oliver
 PACKAGES=$SERVERDIR/download/Python
 DOCS=$SERVERDIR/software/GromacsWrapper
 
+usage="usage: $0 [-h] [cmd1 cmd2 ...]
+
+default: 'distribution docs'
+
+cmd           description
+-----         -----------------
+distribution  make sdist and egg, copy to $PACKAGES
+docs          'make_epydoc make_sphinx'
+make_epydoc   source code docs, copy to $DOCS/epydoc
+make_sphinx   documentation, copy to $DOCS/html
+"
+
+function die () {
+    echo 1>&2 "ERROR: $1"
+    exit ${2:-1}
+}
+
+
 distribution () {
-  python setup.py sdist
-  python setup.py bdist_egg
-  rsync -v dist/* $PACKAGES
+  python setup.py sdist \
+      && python setup.py bdist_egg \
+      && rsync -v dist/* $PACKAGES \
+      || die "Failed distribution"
 }
 
 make_epydocs() {
   epydoc -v -o doc/epydoc --html --name=GromacsWrapper \
          --url=http://sbcb.bioch.ox.ac.uk/oliver/software/GromacsWrapper/ \
-         gromacs gromacs/analysis/plugins/ vmd/
+         gromacs gromacs/analysis/plugins/ vmd/  \
+      || die "Failed making epydoc"
+  rsync -vrP doc/epydoc $DOCS
 }
 
 make_sphinx () {
-  (cd doc/sphinx && make html)
+  (cd doc/sphinx && make html) || die "Failed making sphinx docs"
+  rsync -vrP doc/sphinx/build/html $DOCS
 }
 
 docs () {
   make_epydocs 
   make_sphinx
-  rsync -vrP doc/epydoc $DOCS
-  rsync -vrP doc/sphinx/build/html $DOCS
 }
 
 
+case "$1" in
+    -h|--help) echo "$usage"; exit 0;;
+esac
 
-distribution
-docs
+commands="$@"
+[ -n "$commands" ] || commands="distribution docs"
+
+for cmd in $commands; do
+    eval "$cmd"
+done
+
 
   
