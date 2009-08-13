385b6609e17acb16828a600dc70a52ab473981b5
Oliver Beckstein <orbeckst@gmail.com>
2009-08-13 14:47:37 +0000

    fixed: setup.solvate wrote corrupted include statement to initial mdp
    git-svn-id: svn+ssh://gonzo.med.jhmi.edu/scratch/svn/woolf_repository/users/oliver/Library/GromacsWrapper@3813 df5ba8eb-4b0b-0410-8c14-c10f23b0129c

--- gromacs/setup.py
+++ gromacs/setup.py
@@ -264,7 +264,7 @@ def solvate(struct='top/protein.pdb', top='top/system.top',
         gromacs.genbox(p=topology, cp='boxed.gro', cs=water, o='solvated.gro')
 
         with open('none.mdp','w') as mdp:
-            mdp.write('; empty mdp file\ninclude = %s(include)s\n' % mdp_kwargs)
+            mdp.write('; empty mdp file\ninclude = %(include)s\n' % mdp_kwargs)
 
         qtot = gromacs.cbook.grompp_qtot(f='none.mdp', o='topol.tpr', c='solvated.gro',
                                          p=topology, stdout=False)
