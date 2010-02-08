7004914b297e6c30ee42b12b2b9e56df7a698a94
Oliver Beckstein <oliver.beckstein@bioch.ox.ac.uk>
2010-02-08 00:45:40 +0000

    simple function to get volume from pdb/gro file (uses CRYST or box --- or whatever editconf does)

--- gromacs/cbook.py
+++ gromacs/cbook.py
@@ -461,6 +461,19 @@ def grompp_qtot(*args, **kwargs):
     logger.info("system total charge qtot = %(qtot)r" % vars())
     return qtot
 
+def get_volume(f):
+    """Return the volume in nm^3 of structure file *f*.
+
+    (Uses :func:`gromacs.editconf`; error handling is not good)
+    """
+    fd, temp = tempfile.mkstemp('.gro')
+    try:
+        rc,out,err = gromacs.editconf(f=f, o=temp, stdout=False)
+    finally:
+        os.unlink(temp)
+    return [float(x.split()[1]) for x in out.splitlines()
+            if x.startswith('Volume:')][0]
+
 
 # Editing textual input files
 # ---------------------------
