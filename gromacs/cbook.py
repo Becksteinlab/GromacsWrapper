0d7b09182614cbeff923f03236eeaca60f8f9a5b
Oliver Beckstein <oliver.beckstein@bioch.ox.ac.uk>
2010-02-16 17:09:33 +0000

    sort globbed filenames

--- gromacs/cbook.py
+++ gromacs/cbook.py
@@ -361,7 +361,9 @@ def glob_parts(prefix, ext):
     """Find files from a continuation run"""
     if ext.startswith('.'):
         ext = ext[1:]
-    return glob.glob(prefix+'.'+ext) + glob.glob(prefix+'.part[0-9][0-9][0-9][0-9].'+ext)
+    files = glob.glob(prefix+'.'+ext) + glob.glob(prefix+'.part[0-9][0-9][0-9][0-9].'+ext)
+    files.sort()   # at least some rough sorting...
+    return files
 
 
 class Frames(object):
