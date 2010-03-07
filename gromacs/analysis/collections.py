40fe60a14048287fb7f185521cc2d32aaa1d1364
Oliver Beckstein <oliver.beckstein@bioch.ox.ac.uk>
2010-03-07 00:26:58 +0000

    Collections fully functiona;

--- gromacs/analysis/collections.py
+++ gromacs/analysis/collections.py
@@ -14,18 +14,71 @@ simultaneously for all simulations in the collection.
 .. autoclass:: Collection
 """
 
+import os.path
+import cPickle
+from numpy import all, any
+
 class Collection(list):
-    """Multiple Simulation objects (organized as a list).
+    """Multiple objects (organized as a list).
 
-    Methods are applied to all simulation objects in the Collection.
+    Methods are applied to all objects in the Collection
 
-    Attribute lookup does not work.
+      >>> from gromacs.analysis.collections import Collection
+      >>> animals = Collection(['ant', 'boar', 'ape', 'gnu'])
+      >>> animals.startswith('a')
+      [True, False, True, False]
+
+    Similarly, attributes are returned as a list.
+
+    Using :meth:`Collection.save` one can save the whole collection to
+    disk and restore it later with the :meth:`Collection.load` method
+
+      >>> animals.save('zoo')
+      >>> arc = Collection()
+      >>> arc.load('zoo')
+      >>> arc.load('zoo', append=True)
+      >>> arc
+      ['ant', 'boar', 'ape', 'gnu', 'ant', 'boar', 'ape', 'gnu']
     """
-    # note: do not use with multiple inheritance
+    # note: do not use with multiple inheritance -- why, I think it could work now...
+
+    def save(self, filename):
+        """Pickle the whole collection to *filename*.
+        
+        If no extension is provided, ".collection" is appended.
+        """
+        cPickle.dump(self, open(self._canonicalize(filename), 'wb'), 
+                     protocol=cPickle.HIGHEST_PROTOCOL)
+
+    def load(self, filename, append=False):
+        """Load collection from pickled file *filename*.
+
+        *append* determines if the saved collection is added to the current one
+        or if it replaces the current content.
+
+        If no extension is provided, ".collection" is appended.
+        """
+        tmp = cPickle.load(open(self._canonicalize(filename), 'rb'))
+        if append:
+            self.extend(tmp)
+        else:
+            self[:] = tmp[:]
+        del tmp
+
+    def _canonicalize(self, filename):
+        """Use .collection as extension unless provided"""
+        path, ext = os.path.splitext(filename)
+        if not ext:
+            ext = ".collection"
+        return path + ext
+
+    def __getnewargs__(self, *args, **kwargs):
+        """Provide proper initialization to make pickling with protocol 2 work"""
+        return (self[:],)
 
     def __getattribute__(self, attr):
         try:
-            return list.__getattribute__(self, attr)
+            return super(Collection, self).__getattribute__(attr)
         except AttributeError:
             pass
         
@@ -37,9 +90,20 @@ class Collection(list):
             raise AttributeError("The following members of the collection do not "
                                  "implement the attribute %(attr)r:\n%(failures)r\n"
                                  % vars())
-        def runall(*args, **kwargs):
-            return [o.__getattribute__(attr)(*args, **kwargs) for o in self]                
-        return runall
+
+        # analyze attribute: functions (the ones with __call__) get delayed, simple
+        # attributes are looked up immediately
+        iscallable = [hasattr(o.__getattribute__(attr), '__call__') for o in self]
+        if all(iscallable):
+            def runall(*args, **kwargs):
+                return [o.__getattribute__(attr)(*args, **kwargs) for o in self]
+            runall.__name__ = attr
+            return runall
+        elif any(iscallable):
+            raise TypeError("Attribute %r is callable only for some objects" % attr)
+
+        return [o.__getattribute__(attr) for o in self]
+
 
     def __add__(self, x):
         return Collection(list.__add__(self,x))
