e068c16aa1ee10b755672e3037b225968a155784
Oliver Beckstein <orbeckst@gmail.com>
2012-05-22 17:42:50 -0700

    gw-fit_strip_trajectories.py: option for setting the deffnm prefix

--- scripts/gw-fit_strip_trajectories.py
+++ scripts/gw-fit_strip_trajectories.py
@@ -3,11 +3,11 @@ usage = """usage: %prog [options] RUN_ID [RUN_ID ...]
 
 Process the trajectories associated with RUN_ID, which is something
 like MD_001, MD_002, etc --- essentially the directory name. All files
-are assumed to be named md.* and live in directories *basedir*/RUN_ID.
+are assumed to be named PREFIX.* and live in directories *basedir*/RUN_ID.
 
+Trajectories are generated for timesteps dt 1 ps, 100 ps, and 1000 ps.
 """
-# not used
-JOBNAMES = 'MD_001', 'MD_002', 'MD_003', 'MD_004', 'MD_005'
+
 
 import gromacs
 import gromacs.analysis
@@ -21,8 +21,10 @@ logger = logging.getLogger("gromacs.app")
 
 def MySimulation(identifier, **kwargs):
     basedir = kwargs.pop("basedir", os.path.curdir)
-    def F(ext, identifier=identifier):
-        return os.path.join(basedir, str(identifier), "md.%s"%ext)
+    prefix = kwargs.pop("prefix", "md")
+    def F(ext, identifier=identifier, prefix=prefix):
+        return os.path.join(basedir, str(identifier), 
+                            "{0}.{1}".format(prefix, ext))
     dt = kwargs.pop("dt", [1, 100, 1000])
     kwargs['plugins'] = [StripWater(dt=dt, compact=True, fit="all"), ]
     return Simulation(tpr=F('tpr'), xtc=F('xtc'), edr=F('edr'), **kwargs)
@@ -35,16 +37,22 @@ if __name__ == "__main__":
     parser.add_option("-B", "--basedir", dest="basedir", default=os.curdir,
                       metavar="DIR",
                       help="set basedir to DIR [%default]")
+    parser.add_option("-p", "--prefix", dest="prefix", default="md",
+                      metavar="PREFIX",
+                      help="filenames are constructed from the default "
+                      "prefix as PREFIX.xtc, PREFIX.tpr, etc (equivalent "
+                      "to the -deffnm in Gromacs [%default]")
     opts, args = parser.parse_args()
 
     gromacs.start_logging()
 
+    logger.info("Constructing filenames from prefix %(prefix)r", vars(opts))
     if len(args) == 0:
         logger.warn("No run identifiers were supplid, doing nuffing")
 
     for identifier in args:
         logger.info("Processing %(identifier)r...", vars())
-        S = MySimulation(identifier, basedir=opts.basedir)
+        S = MySimulation(identifier, prefix=opts.prefix, basedir=opts.basedir)
         S.run('StripWater')
         logger.info("Completed %(identifier)r", vars())
 
