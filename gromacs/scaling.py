0c7b281de898bceec033d1b4b40c36da129c8928
Jan Domanski <jan.domanski@nih.gov>
2016-02-26 17:35:42 -0500

    Added missing headers, and docs to script

--- gromacs/scaling.py
+++ gromacs/scaling.py
@@ -1,5 +1,5 @@
 
-# GromacsWrapper: run.py
+# GromacsWrapper: scaling.py
 # Copyright (c) 2009 Oliver Beckstein <orbeckst@gmail.com>
 # Released under the GNU Public License 3 (or higher, your choice)
 # See the file COPYING for details.
--- gromacs/tests/test_basic.py
+++ gromacs/tests/test_basic.py
@@ -1,3 +1,9 @@
+
+# GromacsWrapper: test_basic.py
+# Copyright (c) 2009 Oliver Beckstein <orbeckst@gmail.com>
+# Released under the GNU Public License 3 (or higher, your choice)
+# See the file COPYING for details.
+
 from __future__ import division, absolute_import, print_function
 
 import numpy as np
