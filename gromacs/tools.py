2ff798820470a6a84d18e2d82d217b6cfa7b5e69
Pedro Lacerda <pslacerda@gmail.com>
2016-07-19 16:31:55 -0300

    Merged develop into automatic-tool-loading

--- gromacs/tools.py
+++ gromacs/tools.py
@@ -162,7 +162,8 @@ def tool_factory(clsname, name, driver, base=GromacsCommand):
     """ Factory for GromacsCommand derived types. """
     clsdict = {
         'command_name': name,
-        'driver': driver
+        'driver': driver,
+        '__doc__': property(base._get_gmx_docs)
     }
     return type(clsname, (base,), clsdict)
 
