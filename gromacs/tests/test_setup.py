eb025e29ee936a19fa3dccf30f7a2f8091b2bf2d
Oliver Beckstein <orbeckst@gmail.com>
2018-04-30 01:26:35 -0700

    add tests for functions in gromacs.setup
    - topology()
      (use 1AKE *without* crystal water as test system because
      gmx solvate with crystal water seems to miscount added waters
      (the crystal water count seems to be missing in the topology file
      in the section of the added water --- or maybe I made a mistake
      in setting my options, but in any case, bare protein as input
      works))
    - solvate()
    - energy_minimize()
      (use steepest descent and only emtol = 5000 to run quickly)
    
    These tests only check that the promised files exist but they
    do not look into these files... TODO.

--- gromacs/tests/test_setup.py
+++ gromacs/tests/test_setup.py
@@ -25,5 +25,64 @@ def test_trj_compact_main(tmpdir):
         with open(mdpfile, 'w') as mdp:
             mdp.write('; empty mdp file\nrcoulomb = 1\nrvdw = 1\nrlist = 1\n')
         gromacs.grompp(f=mdpfile, o=tprfile, c=f["struct"], p=f["top"])
-        gromacs.setup.trj_compact_main(s=tprfile, f=f["struct"], o=outfile, input=("protein", "system"))
+        gromacs.setup.trj_compact_main(s=tprfile, f=f["struct"], o=outfile,
+                                       input=("protein", "system"))
         assert os.path.exists(outfile)
+
+@pytest.fixture(scope="session")
+def topology(tmpdir_factory, struct=datafile("1ake_A_protein.pdb")):
+    # note: use protein-only input 1ake_A_protein.pdb because solvation fails
+    #       if crystal waters are included (in 1ake_A.pdb)
+    TMPDIR = tmpdir_factory.mktemp('1ake')
+    with TMPDIR.as_cwd():
+        topol_args = gromacs.setup.topology(struct=struct, ff="oplsaa", water="tip4p")
+    return TMPDIR, topol_args
+
+@pytest.fixture(scope="session")
+def solvate(topology):
+    TMPDIR, topol_args = topology
+    with TMPDIR.as_cwd():
+        solvate_args = gromacs.setup.solvate(concentration=0.15,
+                                             water="tip4p",
+                                             **topol_args)
+    return TMPDIR, solvate_args
+
+@pytest.fixture(scope="session")
+def energy_minimize(solvate, nt=2):
+    # run short energy minimization with cheapest minimizer
+    TMPDIR, solvate_args = solvate
+    with TMPDIR.as_cwd():
+        em_args = gromacs.setup.energy_minimize(mdrun_args={'nt': nt},
+                                                integrator="steep",
+                                                emtol=5000,
+                                                maxwarn=1,
+                                                **solvate_args)
+    return TMPDIR, em_args
+
+
+def test_topology(topology):
+    TMPDIR, topol_args = topology
+    top = topol_args['top']
+    struct = topol_args['struct']
+    posres = topol_args['posres']
+    assert os.path.exists(top)
+    assert os.path.exists(struct)
+    assert os.path.exists(posres)
+    # add more tests for content of files!
+
+def test_solvate(solvate):
+    TMPDIR, solvate_args = solvate
+
+    assert_almost_equal(solvate_args['qtot'], 0.0)
+    assert os.path.exists(solvate_args['struct'])
+    assert os.path.exists(solvate_args['ndx'])
+    assert solvate_args['mainselection'] == '"Protein"'
+    # add more tests for content of files!
+
+def test_energy_minimize(energy_minimize):
+    TMPDIR, em_args = energy_minimize
+    assert os.path.exists(em_args['struct'])
+    assert os.path.exists(em_args['top'])
+    assert em_args['mainselection'] == '"Protein"'
+    # add more tests for content of files!
+
