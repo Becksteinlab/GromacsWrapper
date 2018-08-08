7960c0303cd5fe1fb665b0c551a939bc7e82c0eb
Oliver Beckstein <orbeckst@gmail.com>
2018-08-08 16:17:32 -0700

    improved unicode handling
    - added unicode test for input
    - just test that PopenWithInput can deal with unicode strings
    - convert PopenWithInput to bytestring
    - fixed str handling by using six.text_type

--- gromacs/tests/test_core.py
+++ gromacs/tests/test_core.py
@@ -1,9 +1,12 @@
-# GromacsWrapper: test_example.py
+# -*- coding: utf-8 -*-
+# GromacsWrapper
 # Copyright (c) 2009 Oliver Beckstein <orbeckst@gmail.com>
 # Released under the GNU Public License 3 (or higher, your choice)
 # See the file COPYING for details.
 
 from __future__ import division, absolute_import, print_function
+
+import six
 
 import os.path
 import pytest
@@ -46,12 +49,29 @@ class TestCommand(object):
         assert not out
         assert err
 
-    @pytest.mark.parametrize('inp', ("not_used", ("not", "used")))
+    @pytest.mark.parametrize('inp',
+                             ("not_used",
+                              ("not", "used"),
+                              ("unicode", u"Ångström", u"Planck_constant_over_two_π__ℏ"),
+                             ))
     def test_run_with_input(self, command, inp):
-        rc, out, err = command(input=inp)
+        rc, out, err = command(stdout=False, stderr=False, input=inp)
         assert rc == 0
-        assert not out
+        assert out
         assert not err
+
+    @pytest.mark.parametrize('inp',
+                             ("not_used",
+                              ("not", "used"),
+                              ("unicode", u"Ångström", u"Planck_constant_over_two_π__ℏ"),
+                             ))
+    def test_Popen_with_input(self, command, inp):
+        po = command.Popen(stdout=False, stderr=False, input=inp)
+        inp_string = u"\n".join(gromacs.utilities.asiterable(inp)) + u"\n"
+        if six.PY2:
+            assert po.input == inp_string.encode('utf-8')
+        else:
+            assert po.input == inp_string
 
     def test_help_short(self, command, capsys):
         command.help()
