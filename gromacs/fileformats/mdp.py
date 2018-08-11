a2210a395bd253a90f605f468155e9d12ea455c6
richardjgowers <richardjgowers@gmail.com>
2018-08-11 15:38:18 -0500

    fixes Issue #149
    Writing string MDP files no longer adds spaces

--- gromacs/fileformats/mdp.py
+++ gromacs/fileformats/mdp.py
@@ -23,7 +23,7 @@ from __future__ import absolute_import, with_statement
 import os, errno
 import re
 import warnings
-
+import six
 import numpy
 
 
@@ -154,7 +154,7 @@ class MDP(odict, utilities.FileUtils):
                 else:                  # parameter = value
                     if skipempty and (v == '' or v is None):
                         continue
-                    if hasattr(v, '__iter__'):
-                         mdp.write("{} = {}\n".format(k,' '.join(map(str, v))))
-                    else:
+                    if isinstance(v, six.string_types) or not hasattr(v, '__iter__'):
                         mdp.write("{k!s} = {v!s}\n".format(**vars()))
+                    else:
+                         mdp.write("{} = {}\n".format(k,' '.join(map(str, v))))
