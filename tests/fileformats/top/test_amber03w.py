c408802d2601747cdbc70e6789bbe54a1c57834a
Oliver Beckstein <orbeckst@gmail.com>
2023-09-04 21:42:33 -0400

    xfail specific tests (#236)

--- tests/fileformats/top/test_amber03w.py
+++ tests/fileformats/top/test_amber03w.py
@@ -12,9 +12,12 @@ import gromacs
 from .top import TopologyTest
 from ...datafiles import datafile
 
-@pytest.mark.xfail(gromacs.release().startswith("2022"),
-                   reason="issue https://github.com/Becksteinlab/GromacsWrapper/issues/236")
 class TestAmber03w(TopologyTest):
         processed = datafile('fileformats/top/amber03w/processed.top')
         conf = datafile('fileformats/top/amber03w/conf.gro')
         molecules = ['Protein_chain_A', 'SOL', 'IB+', 'CA', 'CL', 'NA', 'MG', 'K', 'RB', 'CS', 'LI', 'ZN']
+
+        @pytest.mark.xfail(gromacs.release().startswith(("2022", "2023")),
+                           reason="issue #236 https://github.com/Becksteinlab/GromacsWrapper/issues/236")
+        def test_mdrun(self, tmpdir, low_performance):
+                super(TestAmber03w, self).test_mdrun(tmpdir, low_performance)
